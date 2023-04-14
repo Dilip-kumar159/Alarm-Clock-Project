adb3c6e313cc1ad843d77cdddc0bab1a9a2f67c1
Dilip <dilip45377@gmail.com>
2023-04-14 21:26:37 +0530

    modified all the files and added music

--- index.js
+++ index.js
@@ -1,107 +1,104 @@
-var sound = new Audio("iphone_alarm.mp3")
-sound.loop = true;
+let arr = [];
+let setHr = document.getElementById("hr");
+let setMin = document.getElementById("min");
+let setSec = document.getElementById("sec");
+let setAMPM = document.getElementById("am-pm");;
+let colorChange = document.getElementsByClassName("time");
+let incompleteAlarmsholder = document.getElementById("pending-alarms");
 
-let arr = []
-let setHr = document.getElementById("hr")
-let setMin = document.getElementById("min")
-let setSec = document.getElementById("sec")
-let setAMPM = document.getElementById("am-pm")
-let colorChange = document.getElementsByClassName("time")
-let incompleteAlarmsholder = document.getElementById("pending-alarms")
+var alarmMusic = new Audio('alarmSound.mp3');
+alarmMusic.loop = true;
 
-
-// Selected by the visitor :
-
-let totalHrs = 12
-// Range from 1 to 12 hrs
+// For Hours
+let totalHrs = 12;
 for (let i = 1; i <= totalHrs; i++) {
-    setHr.options[setHr.options.length] = new Option(i < 10 ? '0' + i : i)
+    setHr.options[setHr.options.length] = new Option(i < 10 ? '0' + i : i);
 }
 
-let totalMins = 59
-// Range of 00-59 minutes
+// For Minutes
+let totalMins = 59;
 for (let i = 0; i <= totalMins; i++) {
-    setMin.options[setMin.options.length] = new Option(i < 10 ? '0' + i : i)
+    setMin.options[setMin.options.length] = new Option(i < 10 ? '0' + i : i);
 }
 
-let totalSecs = 59
-// Range of 00-59 seconds
+// For Seconds
+let totalSecs = 59;
 for (let i = 0; i <= totalSecs; i++) {
-    setSec.options[setSec.options.length] = new Option(i < 10 ? '0' + i : i)
+    setSec.options[setSec.options.length] = new Option(i < 10 ? '0' + i : i);
 }
 
-let morningornoon = ["AM", "PM"]
-// setting the AM, PM
+// set for am & pm
+let morningornoon = ["AM", "PM"];
 for (let i = 0; i < morningornoon.length; i++) {
-    setAMPM.options[setAMPM.options.length] = new Option(morningornoon[i])
+    setAMPM.options[setAMPM.options.length] = new Option(morningornoon[i]);
 }
 
 
-// Function to display the time:
+// display time function:
 function realTime() {
-    const date = new Date()
-    let hr = date.getHours()
-    let min = date.getMinutes()
-    let sec = date.getSeconds()
-    let AMPM = ""
+    const date = new Date();
+    let hr = date.getHours();
+    let min = date.getMinutes();
+    let sec = date.getSeconds();
+    let AMPM = "";
 
     if (date.getHours() == 0) {
-        hr = 12
+        hr = 12;
     }
 
     if (date.getHours() >= 12) {
         if (date.getHours() == 12) {
-            hr = 12
+            hr = 12;
         } else {
-            hr = hr - 12
+            hr = hr - 12;
         }
 
-        AMPM = "PM"
+        AMPM = "PM";
     } else {
-        AMPM = "AM"
+        AMPM = "AM";
     }
 
     if (hr.toString().length == 1) {
-        hr = '0' + hr
+        hr = '0' + hr;
     }
     if (min < 10) {
-        min = '0' + min
+        min = '0' + min;
     }
     if (sec < 10) {
-        sec = '0' + sec
+        sec = '0' + sec;
     }
 
-    document.getElementById("realtime").innerHTML = hr + ":" + min + ":" + sec + AMPM
+    document.getElementById("real").innerHTML = hr + ":" + min + ":" + sec + AMPM;
 }
-setInterval(realTime, 1000)
+setInterval(realTime, 1000);
 
 
-// Creating new Alarm time element in form of list:
+// New Alarm Time in form of list
 var createNewTaskElement = function (alarmString) {
-    let listItem = document.createElement("li")
-    let label = document.createElement("label")
-    let deleteButton = document.createElement("button")
+    let listItem = document.createElement("li");
+    let label = document.createElement("label");
+    let deleteButton = document.createElement("button");
 
-    deleteButton.innerText = "Delete" + alarmString[0]
-    deleteButton.className = "delete"
-    label.innerText = alarmString
+    deleteButton.innerText = "Delete" + alarmString[0];
+    deleteButton.className = "delete";
+    label.innerText = alarmString;
 
-    listItem.appendChild(label)
-    listItem.appendChild(deleteButton)
-    return listItem
+    listItem.appendChild(label);
+    listItem.appendChild(deleteButton);
+    return listItem;
 }
 
 
-// Clicking the alarm time button which triggers creating the alarm time element:
+// alarm time button which triggers creating the alarm time element:
 document.getElementById("set-button").addEventListener("click", function () {
     let selectedHr = setHr.options[setHr.selectedIndex].value;
     let selectedMin = setMin.options[setMin.selectedIndex].value;
     let selectedSec = setSec.options[setSec.selectedIndex].value;
     let selectedAMPM = setAMPM.options[setAMPM.selectedIndex].value;
-    console.log(selectedHr, selectedMin, selectedSec, selectedAMPM)
-    let len = arr.length + 1
+    console.log(selectedHr, selectedMin, selectedSec, selectedAMPM);
+    let len = arr.length + 1;
 
-    // Getting today's time
+    // Getting real time
     var today = new Date();
     var dd = String(today.getDate()).padStart(2, '0');
     var mm = String(today.getMonth() + 1).padStart(2, '0'); //January is 0!
@@ -110,95 +107,97 @@ document.getElementById("set-button").addEventListener("click", function () {
     today = mm + '/' + dd + '/' + yyyy;
     let alarmhr = parseInt(selectedHr)
     if (selectedAMPM == "PM") {
-        alarmhr = 12 + alarmhr
+        alarmhr = 12 + alarmhr;
     }
 
     if (selectedAMPM == "AM" && alarmhr == 12) {
-        alarmhr = 0
+        alarmhr = 0;
     }
     if (alarmhr.toString.length == 1) {
-        alarmhr = '0' + alarmhr
+        alarmhr = '0' + alarmhr;
     }
     let timeForAlarm = alarmhr + ":" + selectedMin + ":" + selectedSec
     var d = new Date(`${today} ${timeForAlarm}`);
 
-    // Getting time in milliseconds 
+    // time in miliseconds
     var milliseconds = d.getTime();
 
     // storing alarm time data in an array
-    arr.push([selectedHr, selectedMin, selectedSec, selectedAMPM, milliseconds, len])
+    arr.push([selectedHr, selectedMin, selectedSec, selectedAMPM, milliseconds, len]);
 
     // milliseconds time is used for sorting the array and the first element in the array
     // will be the first alarm to get triggered
-    arr = arr.sort((a, b) => a[4] - b[4])
-    let val = len.toString() + ") " + selectedHr + ":" + selectedMin + ":" + selectedSec + ":" + selectedAMPM
+    arr = arr.sort((a, b) => a[4] - b[4]);
+    let val = len.toString() + ") " + selectedHr + ":" + selectedMin + ":" + selectedSec + ":" + selectedAMPM;
 
     // creating the alarm list element which includes delete button
-    var listItem = createNewTaskElement(val)
-    incompleteAlarmsholder.appendChild(listItem)
+    var listItem = createNewTaskElement(val);
+    incompleteAlarmsholder.appendChild(listItem);
     // This function is used for deleting an alarm element
-    bindAlarmEvents(listItem)
+    bindAlarmEvents(listItem);
 
     // function to check alarm time with real time
     setInterval(() => {
-        const date = new Date()
-        let hr = date.getHours()
-        let min = date.getMinutes()
-        let sec = date.getSeconds()
-        let AMPM = "AM"
+        const date = new Date();
+        let hr = date.getHours();;
+        let min = date.getMinutes();
+        let sec = date.getSeconds();
+        let AMPM = "AM";
         if (date.getHours() == 0) {
-            hr = 12
+            hr = 12;
         }
 
         if (date.getHours() > 12) {
             if (date.getHours() == 12) {
-                hr = 12
+                hr = 12;
             } else {
-                hr = hr - 12
+                hr = hr - 12;
             }
-            AMPM = "PM"
+            AMPM = "PM";
         } else {
-            AMPM = "AM"
+            AMPM = "AM";
         }
 
         if (hr.toString().length == 1) {
-            hr = '0' + hr
+            hr = '0' + hr;
         }
         if (min < 10) {
-            min = '0' + min
+            min = '0' + min;
         }
         if (sec < 10) {
-            sec = '0' + sec
+            sec = '0' + sec;
         }
 
-        // When real time matches with alarm time, the alarm shows an alert and then starts ringing
+        // When real time matches with alarm time, the alarm shows an alert,
+        // after clicking okay button then music starts to play.
         if (arr.length != 0 && arr[0][0] == hr && arr[0][1] == min && arr[0][2] == sec && arr[0][3] == selectedAMPM) {
-            alert("Alarm is ringing")
-            sound.play()
+            alert("Alarm is started ringing");
+            alarmMusic.play();
         }
-    }, 1000)
+    }, 1000);
 })
 
 // When set clear button is clicked, alarm sound stops
 document.getElementById("clear-button").addEventListener("click", function () {
-    sound.pause()
-})
-var indexDel = 0
+    alarmMusic.pause();
+});
+
+var indexDel = 0;
 
 // To delete  perticular alarm from the recent listed alarms:
 var deleteAlarm = function () {
-    let listItem = this.parentNode
-    var ul = listItem.parentNode
-    var deleteButton = listItem.querySelector("button.delete")
-    indexDel = parseInt(deleteButton.innerHTML[6])
+    let listItem = this.parentNode;
+    var ul = listItem.parentNode;
+    var deleteButton = listItem.querySelector("button.delete");
+    indexDel = parseInt(deleteButton.innerHTML[6]);
     for (let i = 0; i < arr.length; i++) {
         if (arr[i][5] == indexDel) {
-            arr.splice(i, 1)
+            arr.splice(i, 1);
         }
     }
-    ul.removeChild(listItem)
+    ul.removeChild(listItem);
 }
 var bindAlarmEvents = function (alarmListItem) {
-    var deleteButton = alarmListItem.querySelector("button.delete")
-    deleteButton.onclick = deleteAlarm
+    var deleteButton = alarmListItem.querySelector("button.delete");
+    deleteButton.onclick = deleteAlarm;
 }
